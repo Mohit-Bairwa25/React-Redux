e0d7ea524309415e53db9df86507a4d9978866a8
Mohit-Bairwa25 <115634456+Mohit-Bairwa25@users.noreply.github.com>
2024-08-12 18:30:08 +0530

    Redux App Started

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <>
+      <h2>Buy Adidas Shoes &#8377; 5000</h2>
+      <button className="btn btn-primary mx-2">-</button>
+        Add to Cart
+      <button className="btn btn-primary mx-2">+</button>
+    </>
+  )
+}
+
+export default Shop
